ea472dfbc9186ced766d60148a9f5f3a2517e216
theslof <theslof@gmail.com>
2017-12-17 21:33:41 +0100

    Optimized the check loop

--- day13_3.py
+++ day13_3.py
@@ -1,11 +1,5 @@
 import sys
 
-
-def printarray(array):
-    for row in array:
-        print row
-
-
 with open(sys.argv[1], 'r') as fi:
     data = []
     firewall = []
@@ -16,16 +10,16 @@ with open(sys.argv[1], 'r') as fi:
         if data[n][0] > i:
             firewall += [0]
         else:
-            firewall += [(data[n][1]-1) * 2]
+            firewall += [(data[n][1] - 1) * 2]
             n += 1
+    # Checking for shortest delay
     delay = 0
-    match = False
-    while not match:
-        match = True
-        for step in range(0, len(firewall)):
-            print (delay, step, firewall[step])
-            if firewall[step] != 0 and (delay + step) % firewall[step] == 0:
-                match = False
-                delay += 1
-                break
+    step = 0
+    length = len(firewall)
+    while step < length:
+        f = firewall[step]
+        if f != 0 and (delay + step) % f == 0:
+            step = -1
+            delay += 1
+        step += 1
     print delay
