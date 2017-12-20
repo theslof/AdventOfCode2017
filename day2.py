3e1545f89d49d0f191545923ae027970e50dca5d
theslof <theslof@gmail.com>
2017-12-20 20:02:49 +0100

    Added day2 star1

--- day2.py
+++ day2.py
@@ -1,15 +1,4 @@
 import sys
 
 with open(sys.argv[1], 'r') as fi:
-    checksum = 0
-    for line in fi:
-        numbers = map(int, line.split())
-        for n in range (0, len(numbers)-1):
-            first = numbers[n]
-            for m in range (n+1, len(numbers)):
-                second = numbers[m]
-                if first % second == 0:
-                    checksum += first / second
-                if second % first == 0:
-                    checksum += second / first
-print(checksum)
+    print sum([max(row) - min(row) for row in (map(int, row.split()) for row in fi)])
