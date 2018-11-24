d5dc47c3eb9092032b34b97ee6e64cdd2b1056f3
theslof <theslof@gmail.com>
2018-11-24 20:14:47 +0100

    Updated Day17

---
+++ day17_1.py
@@ -0,0 +1,8 @@
+step = int(open('day17_data', 'r').readline())
+spinlock = [0, 1]
+index = 1
+cycles = 2017
+for i in range(2, cycles + 1):
+    index = (index + step) % i + 1
+    spinlock = spinlock[0:index] + [i] + spinlock[index:]
+print(spinlock[index + 1])
--- day17_2.py
+++ day17_2.py
@@ -2,17 +2,13 @@
 # Therefore we don't actually have to keep track of any list, we only have
 # to do our computations and save the current value whenever the index lands
 # on the second element.
-# I'm hungry, so I may be missing some major loop-free optimization.
 
-import sys
-
-with open(sys.argv[1], 'r') as fi:
-    index = 1
-    cycles = 50000000
-    step = int(fi.readline())
-    solution = 1
-    for i in range(2, cycles + 1):
-        index = (index + step) % i + 1
-        if index - 1 == 0:
-            solution = i
-    print solution
+index = 1
+cycles = 50000000
+step = int(open('day17_data', 'r').readline())
+solution = 1
+for i in range(2, cycles + 1):
+    index = (index + step) % i + 1
+    if index == 1:
+        solution = i
+print(solution)
