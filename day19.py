a73d562e8400e2ae4e6553653726aae708907d01
theslof <theslof@gmail.com>
2018-11-24 20:57:49 +0100

    Updated Day19

--- day19.py
+++ day19.py
@@ -1,17 +1,11 @@
-import sys
-
-
-def printarray(array):
-    for row in array:
-        print row
-
-
-with open(sys.argv[1], 'r') as fi:
+with open('day19_data', 'r') as fi:
     letters = list()
     grid = list()
     steps = 0
     for row in fi:
-        grid.append(row)
+        grid.append(row + ' ')
+    # We appended a space to every row and append a blank row as the last in the list to avoid index out of range errors
+    grid.append(''.join([' ' for _ in range(len(grid[-1]))]))
     x = grid[0].index('|')
     y = 0
     dirx = 0
@@ -21,10 +15,11 @@ with open(sys.argv[1], 'r') as fi:
             break
         if x < 0 or x >= len(grid[y]):
             break
-        if grid[y][x] == ' ':
+        cell = grid[y][x]
+        if cell == ' ':
             break
         steps += 1
-        if grid[y][x] == '+':
+        if cell == '+':
             if diry != 0:
                 if grid[y][x + 1] != '|' and grid[y][x + 1] != ' ':
                     diry = 0
@@ -32,17 +27,17 @@ with open(sys.argv[1], 'r') as fi:
                 if grid[y][x - 1] != '|' and grid[y][x - 1] != ' ':
                     diry = 0
                     dirx = -1
-            else:
-                if dirx != 0:
-                    if grid[y + 1][x] != '-' and grid[y + 1][x] != ' ':
-                        diry = 1
-                        dirx = 0
-                    if grid[y - 1][x] != '-' and grid[y - 1][x] != ' ':
-                        diry = -1
-                        dirx = 0
-        if 64 < ord(grid[y][x]) < 91:
-            letters.append(grid[y][x])
+            elif dirx != 0:
+                if grid[y + 1][x] != '-' and grid[y + 1][x] != ' ':
+                    diry = 1
+                    dirx = 0
+                if grid[y - 1][x] != '-' and grid[y - 1][x] != ' ':
+                    diry = -1
+                    dirx = 0
+        if 64 < ord(cell) < 91:
+            # We found a letter, add it!
+            letters.append(cell)
         y += diry
         x += dirx
-    print ''.join(letters)
-    print steps
+    print(''.join(letters))
+    print(steps)
