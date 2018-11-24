9b6aac778cc32d15dccb0ee75a4e10ef7cae3c53
theslof <theslof@gmail.com>
2018-11-24 22:11:05 +0100

    Updated Day20

---
+++ day20_1.py
@@ -0,0 +1,23 @@
+import re
+
+
+def manhattan(steps, p, v, a):
+    cx = p[0] + steps * (v[0] + steps * a[0])
+    cy = p[1] + steps * (v[1] + steps * a[1])
+    cz = p[2] + steps * (v[2] + steps * a[2])
+    return int(abs(cx) + abs(cy) + abs(cz))
+
+
+with open('day20_data', 'r') as fi:
+    data = list()
+    i = 0
+    for row in fi:
+        p, v, a, *_ = re.findall('<(.*?)>', row)
+        pl = list(map(int, p.split(',')))
+        vl = list(map(int, v.split(',')))
+        al = list(map(int, a.split(',')))
+        data.append((i, manhattan(1000, pl, vl, al)))
+        i += 1
+
+    dist = [(d, i) for i, d in data]
+    print(min(dist)[1])
--- day20_2.py
+++ day20_2.py
@@ -1,9 +1,8 @@
 # A really dirty implementation. It eliminates any colliding pixels each step, reducing the number of checks.
-# Still far too many checks and loops, slow as hell (several seconds for 1000 loops).
-# There must be some fancy mathematical solution to see if two vectors collide at time t, but vector algebra is not
-# my strong suit and I should study for a test instead.
+# Slow and clunky, but I implemented a bail-out counter. If no collisions have been detected for n iterations, quit.
+import re
 
-import sys
+bailout = 20
 
 
 class Pixel:
@@ -13,49 +12,46 @@ class Pixel:
         self.a = [0, 0, 0]
 
     def __str__(self):
-        return '(' + str(self.coords[0]) + ',' + str(self.coords[1]) + ',' + str(self.coords[2]) + ')'
+        return '<' + str(self.coords[0]) + ',' + str(self.coords[1]) + ',' + str(self.coords[2]) + '>' \
+              ' <' + str(self.v[0]) + ',' + str(self.v[1]) + ',' + str(self.v[2]) + '>' \
+              ' <' + str(self.a[0]) + ',' + str(self.a[1]) + ',' + str(self.a[2]) + '>'
 
 
-def step(pixel):
-    for i in range(0, 3):
-        pixel.v[i] += pixel.a[i]
-        pixel.coords[i] += pixel.v[i]
+def step(data: [Pixel]):
+    for pixel in data:
+        for i in range(3):
+            pixel.v[i] += pixel.a[i]
+            pixel.coords[i] += pixel.v[i]
 
 
-def checkcollision(array):
-    match = True
-    while match:
-        match = False
-        for pixel in set(array):
-            deadpixels = [p for p in array if p.coords == pixel.coords]
-            if len(deadpixels) > 1:
-                map(array.remove, deadpixels)
-                match = True
-                break
+def checkcollision(array: {Pixel}):
+    deadpixels = set()
+    for index, pixel in enumerate(array):
+        for p in array:
+            if p.coords == pixel.coords and p != pixel:
+                deadpixels.add(p)
+    return deadpixels
 
 
-with open(sys.argv[1], 'r') as fi:
+with open('day20_data', 'r') as fi:
     data = set()
     for row in fi:
         p = Pixel()
-        start = row.index('<')
-        end = row.index('>')
-        p.coords = map(int, row[start + 1:end].split(','))
-        start = row[end:].index('<') + end
-        end = row[start:].index('>') + start
-        p.v = map(int, row[start + 1:end].split(','))
-        start = row[end:].index('<') + end
-        end = row[start:].index('>') + start
-        p.a = map(int, row[start + 1:end].split(','))
+        pl, vl, al, *_ = re.findall('<(.*?)>', row)
+        p.coords = list(map(int, pl.split(',')))
+        p.v = list(map(int, vl.split(',')))
+        p.a = list(map(int, al.split(',')))
         data.add(p)
-    #        print p
-    l = len(data)
-    print l
-    for i in range(0, 1000):
-        if len(data) <= 1:
+    print('Checking collisions...')
+    i = 0
+    for _ in range(10000):
+        dead = checkcollision(data)
+        if len(dead):
+            data -= dead
+            i = 0
+        else:
+            i += 1
+        if i > bailout:
             break
-        checkcollision(data)
-        map(step, data)
-        if len(data) < l:
-            l = len(data)
-    print l
+        step(data)
+    print(len(data))
